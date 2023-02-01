502612d6a59636d10e93dd8f5957afaeda7ee900
Elena <ebromeo88@gmail.com>
2023-02-02 00:55:46 +0100

    Tomorrow more

--- src/mocks/tasks.ts
+++ src/mocks/tasks.ts
@@ -1,6 +1,6 @@
 import { Pet, PetStructure } from '../models/pet';
 
-export const PET: PetStructure[] = [
+export const TASK: PetStructure[] = [
   { id: '164983', name: 'Mellow', isAdopted: false, owner: 'Pepe' },
   new Pet('Oddie', 'Jack Russel', 'Luis'),
   new Pet('Loba', 'Mestizo', 'Elena'),
